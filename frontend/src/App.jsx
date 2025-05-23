0c36b71db2672d4cd21fc35ea82fc996527f5609
Fuz7 <124787267+Fuz7@users.noreply.github.com.>
2025-05-23 15:19:50 +0800

    Add all necessary frontendAssets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 function App() {
 
   return (
-    <div className='w-[300px] h-[400px] bg-red-400'>
+    <div className='w-[300px] h-[400px] text-[50px] font-poppins'>
+      Poppin
     </div>
   )
 }
