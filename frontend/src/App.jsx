5a521503cddbe9d130d3f4121113d59102f8b90e
Fuz7 <124787267+Fuz7@users.noreply.github.com.>
2026-09-09 12:21:54 +0800

    chore: test wf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
       >
         <main className=" w-full min-h-full glassy px-[65px] py-[50px]">
           <h1 className="font-poppins font-bold text-primary text-[50px] leading-none mb-[40px]">
-            Eri Pharmacy Staging Real
+            Eri Pharmacy Staging Real Ah
           </h1>
           <Header
             productFilters={productFilters}
